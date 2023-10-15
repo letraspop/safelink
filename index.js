313a892a15f1953e16624a83cd80447e393ec514
ilmailfiyah <irawannnria85@gmail.com>
2023-10-15 10:07:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -312,12 +312,3 @@ shortcut = {
         setTimeout(t, 5e3)
     }
 }();
-
-/* copas */
-function nocopas() {
-    var e = window.getSelection();
-    pagelink = " Read more : " + document.location.href, copytext = e + pagelink, newdiv = document.createElement("div"), newdiv.style.position = "absolute", newdiv.style.left = "-99999px", document.body.appendChild(newdiv), newdiv.innerHTML = copytext, e.selectAllChildren(newdiv), window.setTimeout(function () {
-        document.body.removeChild(newdiv)
-    }, 100)
-}
-document.addEventListener("copy", nocopas);
